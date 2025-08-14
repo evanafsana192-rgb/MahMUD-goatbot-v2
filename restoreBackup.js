9a05f9b0cd83fcb82bfd663b353641da9c65ca1c
evanafsana192-rgb <evanafsana192@gmail.com>
2025-08-14 22:52:19 +0600

    Update restoreBackup.js

--- restoreBackup.js
+++ restoreBackup.js
@@ -1,66 +1,119 @@
-const fs = require("fs-extra");
-const readline = require("readline");
-const log = require('./logger/log.js');
-
-let versionBackup;
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
-});
-
-function recursiveReadDirAndBackup(pathFileOrFolder) {
-	const pathFileOrFolderBackup = `${process.cwd()}/${versionBackup}/${pathFileOrFolder}`;
-	const pathFileOrFolderRestore = `${process.cwd()}/${pathFileOrFolder}`;
-
-	if (fs.lstatSync(pathFileOrFolderBackup).isDirectory()) {
-		if (!fs.existsSync(pathFileOrFolderRestore))
-			fs.mkdirSync(pathFileOrFolderRestore);
-		const readDir = fs.readdirSync(pathFileOrFolderBackup);
-		readDir.forEach(fileOrFolder => {
-			recursiveReadDirAndBackup(`${pathFileOrFolder}/${fileOrFolder}`);
-		});
+[
+	{
+		"key": "dbln",
+		"value": "%7B%22100069002860471%22%3A%22HRH4FX6c%22%7D",
+		"domain": "facebook.com",
+		"path": "/login/device-based/",
+		"hostOnly": false,
+		"creation": "2025-08-14T16:17:36.820Z",
+		"lastAccessed": "2025-08-14T16:17:36.828Z"
+	},
+	{
+		"key": "datr",
+		"value": "TPmdaJAg3mdQ8RZ31gFYEe1H",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-08-14T16:17:36.828Z",
+		"lastAccessed": "2025-08-14T16:17:36.828Z"
+	},
+	{
+		"key": "sb",
+		"value": "TPmdaElASL1DpSeyRniti8qM",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-08-14T16:17:36.828Z",
+		"lastAccessed": "2025-08-14T16:17:36.828Z"
+	},
+	{
+		"key": "ps_l",
+		"value": "1",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-08-14T16:17:36.828Z",
+		"lastAccessed": "2025-08-14T16:17:36.828Z"
+	},
+	{
+		"key": "ps_n",
+		"value": "1",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-08-14T16:17:36.828Z",
+		"lastAccessed": "2025-08-14T16:17:36.828Z"
+	},
+	{
+		"key": "c_user",
+		"value": "100069002860471",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-08-14T16:17:36.828Z",
+		"lastAccessed": "2025-08-14T16:17:36.828Z"
+	},
+	{
+		"key": "xs",
+		"value": "36%3Ay3D8hO9e2hQZrg%3A2%3A1755183460%3A-1%3A-1",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-08-14T16:17:36.828Z",
+		"lastAccessed": "2025-08-14T16:17:36.828Z"
+	},
+	{
+		"key": "locale",
+		"value": "bn_IN",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-08-14T16:17:36.828Z",
+		"lastAccessed": "2025-08-14T16:17:36.828Z"
+	},
+	{
+		"key": "pas",
+		"value": "100069002860471%3Ax1J4AdQWFM",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-08-14T16:17:36.828Z",
+		"lastAccessed": "2025-08-14T16:17:36.828Z"
+	},
+	{
+		"key": "vpd",
+		"value": "v1%3B805x411x2.6312501430511475",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-08-14T16:17:36.829Z",
+		"lastAccessed": "2025-08-14T16:17:36.829Z"
+	},
+	{
+		"key": "fr",
+		"value": "01PiRS8W79HjWpHfF.AWegdCnW5Tomgr0BYSSOPcbfvofrjFvpe3OZ8zyFZ4RckR1Y8oI.BonflM..AAA.0.0.Bongvt.AWcB-z13YtnkRcvRLlY1Ia8uBm0",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-08-14T16:17:36.829Z",
+		"lastAccessed": "2025-08-14T16:17:36.829Z"
+	},
+	{
+		"key": "wl_cbv",
+		"value": "v2%3Bclient_version%3A2893%3Btimestamp%3A1755188205",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-08-14T16:17:36.829Z",
+		"lastAccessed": "2025-08-14T16:17:36.829Z"
+	},
+	{
+		"key": "fbl_st",
+		"value": "101736376%3BT%3A29253136",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-08-14T16:17:36.829Z",
+		"lastAccessed": "2025-08-14T16:17:36.829Z"
 	}
-	else {
-		pathFileOrFolder = pathFileOrFolder
-			.replace(/\\/g, '/');
-		fs.copyFileSync(pathFileOrFolderBackup, pathFileOrFolderRestore);
-	}
-}
-
-(async () => {
-	if (process.argv.length < 3) {
-		versionBackup = await new Promise((resolve) => {
-			rl.question("Input version backup: ", answer => {
-				resolve(answer);
-			});
-		});
-	}
-	else {
-		versionBackup = process.argv[2];
-	}
-
-	if (!versionBackup) {
-		log.error("ERROR", `Please input version backup`);
-		process.exit();
-	}
-
-	versionBackup = versionBackup.replace("backup_", ""); // remove backup_ if exists (may be user input backup_1.0.0)
-	versionBackup = `backup_${versionBackup}`;
-
-	const backupFolder = `${process.cwd()}/backups/${versionBackup}`;
-	if (!fs.existsSync(backupFolder)) {
-		log.error("ERROR", `Backup folder is not exists (${backupFolder})`);
-		process.exit();
-	}
-
-	const files = fs.readdirSync(backupFolder);
-	for (const file of files)
-		recursiveReadDirAndBackup(file);
-
-	const packageJson = require(`${process.cwd()}/package.json`);
-	packageJson.version = versionBackup.replace("backup_", "");
-	fs.writeFileSync(`${process.cwd()}/package.json`, JSON.stringify(packageJson, null, 2));
-
-	log.info("SUCCESS", `Restore backup ${versionBackup} success`);
-})();
-
+]
